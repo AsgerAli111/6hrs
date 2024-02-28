b6d48dc887e4a689b8feede91fea1f52ebebf65e
AsgerAli <asger1312@gmail.com>
2024-02-28 22:11:31 +0530

    typedef

--- code/firstcode.cpp
+++ code/firstcode.cpp
@@ -10,18 +10,17 @@ namespace second{
     float x=34;
 }
 
-
-
-
-
+typedef std::string text_t;
 
 int main(){
 
     int x=10;
+    text_t name ={"asger"};
 
     std::cout<<"local x: "<<x<<std::endl;
     std::cout<<"namespace x: "<<first::x<<std::endl;
-        std::cout<<"namespace x: "<<second::x<<std::endl;
+    std::cout<<"namespace x: "<<second::x<<std::endl;
+    std::cout<<name;
 
 
 
