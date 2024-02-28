a646728a72b27eee839395e80298d83ec739f6e2
AsgerAli <asger1312@gmail.com>
2024-02-28 22:01:48 +0530

    namespace

--- code/firstcode.cpp
+++ code/firstcode.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 
+namespace first{
 
+    int x=20;
+}
+
+namespace second{
+
+    float x=34;
+}
 
 
 
@@ -8,6 +16,12 @@
 
 
 int main(){
+
+    int x=10;
+
+    std::cout<<"local x: "<<x<<std::endl;
+    std::cout<<"namespace x: "<<first::x<<std::endl;
+        std::cout<<"namespace x: "<<second::x<<std::endl;
 
 
 
